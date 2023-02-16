5fbd5ea31d6e781dea7793eb350ef2633bd616e5
Odia <94786718+AustinAdodo@users.noreply.github.com>
2023-02-16 14:23:29 +0100

    Crazy Brackets Completed. :)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CrazyBrackets
+﻿using System.IO.Pipes;
+
+namespace CrazyBrackets
 {
     internal class Program
     {
@@ -10,57 +12,52 @@
             {
                 if (value <= 0)
                     return new int[][] { new int[0] };
-
                 if (startWith < 0)
                     startWith = value - 1;
-
                 List<int[]> solutions = new List<int[]>();
-
                 for (int i = Math.Min(value, startWith); i >= 1; --i)
                     foreach (int[] solution in GetCombination2(value - i, i))
                     {
                         int[] next = new int[solution.Length + 1];
-
                         Array.Copy(solution, 0, next, 1, solution.Length);
                         next[0] = i;
-
                         solutions.Add(next);
                     }
 
                 // Or just (if we are allow a bit of Linq)
                 //   return solutions.ToArray();
                 int[][] answer = new int[solutions.Count][];
-
                 for (int i = 0; i < solutions.Count; ++i) answer[i] = solutions[i];
-
                 return answer;
                 //string report = string.Join(Environment.NewLine, result
                 //.Select(solution => string.Join(" + ", solution)));
             }
             //output 3 -> [3, 3(Modified), 2 1, 1 2, 1 1 1 ]
-            public static void CrazyBrackets(int n) //output 3 -> [((())), (()()), (())(), ()(()), ()()() ]
+            public static List<string> CrazyBrackets(int n) //output 3 -> [((())), (()()), (())(), ()(()), ()()() ]
             {
                 List<List<int>> combinations = new List<List<int>>();
                 string[] t = new string[] {"()","(())","((()))","(((())))","((((()))))","(((((())))))","((((((()))))))",
             "(((((((())))))))"};
                 string[] Modified = new string[] {"()","(())","(()())","(()()())","(()()()())","(()()()()())","(()()()()()())",
             "(()()()()()()())"};
-                int[][] njagged = GetCombination2(n);
-                for (int i = 0; i < njagged.Count(); i++)
+                int[][] result = brackets.GetCombination2(n);
+                List<string> answer = new(); answer.Add(t[n - 1]);
+                answer.Add(Modified[n - 1]);
+                foreach (var row in result)
                 {
-                    for (int j = 0; j < njagged.GetLength(i); j++)
+                    for (int i = 0; i < row.Length; i++)
                     {
+                        answer.Add(t[row[i] - 1]);
+                        if (i + 1 <= row.Length - 1 && row[i] != row[i + 1]) { answer.Add(t[row[i] - 1]); answer.Add(t[row[i + 1] - 1]); }
                     }
                 }
+                return answer;
             }
         }
         static void Main(string[] args)
         {
-            int[][] result = brackets.GetCombination2(4);
-            foreach (var row in result)
-            {
-                    Console.Write(String.Join(",", row));  
-            }
+            List<string> result = brackets.CrazyBrackets(3);
+            Console.Write(String.Join(" ", result));
         }
     }
 }
